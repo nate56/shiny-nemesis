5387f01b8ea95042680e817f7e475a389044cf31
nate56 <mageenate@gmail.com>
2014-01-22 08:48:44 -0600

    Create myprogram.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+####################################################
+# program: hello
+# author: Nathan Magee
+# version: 1.0
+# date: 01/22/14
+# description: this program does amazing things
+# and is a simple social network where people can 
+# post what they're doing and where. 140 char limit.
+####################################################
